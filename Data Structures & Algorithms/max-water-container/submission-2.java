60c7ec2758753acd73269dcb98b0c911271e26f4
Venkateshwaran M <83813738+venky-22@users.noreply.github.com>
2026-08-13 00:19:31 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {//Two Pointer Approach 
+                //Time: O(N)
+                //Space: O(1)
+    public int maxArea(int[] heights) {
+        int area = 0;
+        int left = 0;
+        int right = heights.length-1;
+
+        while(left<right){
+            int width = right - left;
+
+            int height = Math.min(heights[right], heights[left]);
+
+            int currentArea = width*height;
+
+            area = Math.max(area, currentArea);
+
+            if(heights[left]< heights[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+
+        return area;
+    }
+
+}
